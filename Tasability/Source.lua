1903982e787fd4d75b62931fbedd74d22c0d9834
NymeraAnHomie <frostiesnowman439@gmail.com>
2025-04-21 16:26:22 +0800

    Update Source.lua

--- Tasability/Source.lua
+++ Tasability/Source.lua
@@ -438,31 +438,6 @@ do
 				end
 			end)
 			
-			task.spawn(function()
-				while true do
-					if States.Reading then
-						if HumanoidState == "Running" and Humanoid:GetState() ~= Enum.HumanoidStateType.Running then
-							Humanoid:ChangeState(Enum.HumanoidStateType.Running)
-						elseif HumanoidState == "Seated" and Humanoid:GetState() ~= Enum.HumanoidStateType.Seated then
-							Humanoid:ChangeState(Enum.HumanoidStateType.Seated)
-						elseif HumanoidState == "Climbing" and Humanoid:GetState() ~= Enum.HumanoidStateType.Climbing then
-							Humanoid:ChangeState(Enum.HumanoidStateType.Climbing)
-						elseif HumanoidState == "Jumping" and Humanoid:GetState() ~= Enum.HumanoidStateType.Jumping then
-							Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-						elseif HumanoidState == "Freefall" and Humanoid:GetState() ~= Enum.HumanoidStateType.Freefall then
-							Humanoid:ChangeState(Enum.HumanoidStateType.Freefall)
-						elseif HumanoidState == "FallingDown" and Humanoid:GetState() ~= Enum.HumanoidStateType.FallingDown then
-							Humanoid:ChangeState(Enum.HumanoidStateType.FallingDown)
-						elseif HumanoidState == "Swimming" and Humanoid:GetState() ~= Enum.HumanoidStateType.Swimming then
-							Humanoid:ChangeState(Enum.HumanoidStateType.Swimming)
-						end
-					else
-						Humanoid:ChangeState(Enum.HumanoidStateType.None)
-					end
-					RunService.RenderStepped:Wait()
-				end
-			end)
-			
 			LocalPlayer.Chatted:Connect(function(message)
 		        local args = string.split(message, " ")
 		        if args[1] == "/e" and AnimNames[args[2]] then
