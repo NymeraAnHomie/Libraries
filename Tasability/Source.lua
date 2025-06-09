93a92699fc62930bcafa5025ef9b17b74a51ff89
NymeraAnHomie <frostiesnowman439@gmail.com>
2025-06-09 10:00:49 +0800

    Update Source.lua

--- Tasability/Source.lua
+++ Tasability/Source.lua
@@ -74,20 +74,26 @@ local Camera = Workspace.CurrentCamera
 local GuiInset = GuiService:GetGuiInset()
 
 local InputCodes = {
-	A = true, B = true, C = true, D = true, E = true, F = true, G = true,
-	H = true, I = true, J = true, K = true, L = true, M = true, N = true,
-	O = true, P = true, Q = true, R = true, S = true, T = true, U = true,
-	V = true, W = true, X = true, Y = true, Z = true,
+	A = true, B = true, C = true, D = true, E = true, F = true,
+	G = true, H = true, I = true, J = true, K = true, L = true,
+	M = true, N = true, O = true, P = true, Q = true, R = true,
+	S = true, T = true, U = true, V = true, W = true, X = true,
+	Y = true, Z = true,
+
+	["0"] = true, ["1"] = true, ["2"] = true, ["3"] = true, ["4"] = true,
+	["5"] = true, ["6"] = true, ["7"] = true, ["8"] = true, ["9"] = true,
 
 	Space = true,
+	Tab = true,
+	Enter = true,
+	Backspace = true,
 	LeftShift = true,
 	RightShift = true,
 
-	MB1 = true,
-	MB2 = true,
-	MB3 = true,
-	MB4 = true,
-	MB5 = true,
+	MB1 = true, -- MouseButton1 = Left Click
+	MB2 = true, -- MouseButton2 = Right Click
+	MB3 = true, -- MouseButton3 = Middle Click
+
 	ScrollUp = true,
 	ScrollDown = true,
 }
